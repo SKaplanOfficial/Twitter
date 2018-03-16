82d04946cd127782f42e41bd6b184132aaa2dc56
Stephen Kaplan <nintendopup@icloud.com>
2018-03-15 20:58:57 -0400

    Create credentials.py

---
+++ Creative-Code-Bot/credentials.py
@@ -0,0 +1,4 @@
+CONSUMER_KEY = 'KEY'#keep quotes, replace with consumer key
+CONSUMER_SECRET = 'SECRET'#keep quotes, replace with consumer secret key
+ACCESS_KEY = 'TOKEN'#keep quotes, replace with access token
+ACCESS_SECRET = 'TOKEN SECRET'#keep quotes, replace with access token secret
