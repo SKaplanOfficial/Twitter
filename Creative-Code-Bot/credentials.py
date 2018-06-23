33eefe8d142d31228f652bd592debe81da48401a
Stephen Kaplan <nintendopup@icloud.com>
2018-06-23 17:00:33 -0400

    Update credentials.py

--- Creative-Code-Bot/credentials.py
+++ Creative-Code-Bot/credentials.py
@@ -2,3 +2,5 @@ CONSUMER_KEY = 'KEY'#keep quotes, replace with consumer key
 CONSUMER_SECRET = 'SECRET'#keep quotes, replace with consumer secret key
 ACCESS_KEY = 'TOKEN'#keep quotes, replace with access token
 ACCESS_SECRET = 'TOKEN SECRET'#keep quotes, replace with access token secret
+OATH_KEY = 'OATH KEY'#keep quotes, replace with oath key
+OATH_SECRET = 'OATH SECRET'#keep quotes, replace with oath secret
