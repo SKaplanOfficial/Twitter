ad9f5853d660a1fb735b6a9e93189a82e06170ce
Stephen Kaplan <nintendopup@icloud.com>
2018-06-23 16:59:37 -0400

    Entirely new structure for retweet.py
    Now using Stream API, also removed favoriting as it was against Twitter's developer policy

--- Creative-Code-Bot/retweet.py
+++ Creative-Code-Bot/retweet.py
@@ -1,27 +1,53 @@
-import tweepy
+import tweepy, json
 from time import sleep
 from credentials import *
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
 
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
-api = tweepy.API(auth)
-
-for tweet in tweepy.Cursor(api.search, q='#creativecode').items():
+def doRetweet(id_string):
     try:
-        print('\nTweet by: @' + tweet.user.screen_name)
-
-        # Retweet tweets as they are found
-        tweet.retweet()
-        print('Retweeted the tweet')
-
-        # Favorite the tweet
-        tweet.favorite()
-        print('Favorited the tweet')
-
-        sleep(120)
-
+        print ('Retweeted the tweet.')
+        print('---------------------------\n')
+        # authenticate against the Twitter API
+        api = tweepy.API(auth1)
+        # actually do the retweet
+        api.retweet(id_string)
+        return
     except tweepy.TweepError as e:
         print(e.reason)
+        print('---------------------------\n')
 
-    except StopIteration:
-        break
+
+class MyStreamListener(tweepy.StreamListener):
+
+    def on_data(self, data):
+        # Twitter returns data in JSON format - we need to decode it first
+        decoded = json.loads(data)
+
+        # Also, we convert UTF-8 to ASCII ignoring all bad characters sent by users
+        #print ('@%s: %s' % (decoded['user']['screen_name'], decoded['text'].encode('ascii', 'ignore')))
+        #print ('')
+        # looks at the json and see if we have pic.twitter.com there somewhere
+        tweet_id = decoded['id_str']
+        tweet_user = decoded['user']['screen_name']
+        print('---------------------------\nTweet by: @'+tweet_user)
+        if (tweet_user != "CreativeCodeBot"):
+            print(tweet_id)
+            doRetweet(tweet_id)
+        else:
+            print('Not going to retweet myself!')
+        return True
+
+    def on_error(self, status_code):
+        print('---------------------------\n')
+        print(status_code)
+
+#This handles Twitter authetification and the connection to Twitter 
+l = MyStreamListener()
+auth1 = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth1.set_access_token(OATH_KEY, OATH_SECRET)
+stream = Stream(auth=auth1, listener=l)
+
+#This line filter Twitter Streams to capture data
+stream.filter(track=['#creativecode','#generativeart','#creativecoding','#creativecoding','creativecode','#p5js','#procedural','#generative','#procgen'])
